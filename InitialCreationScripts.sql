359ae0ad12ed7cf941f625fb1801d2c6b6a350a0
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2024-05-01 11:04:28 +0300

    Dbscripts add :partly_sunny:

--- InitialCreationScripts.sql
+++ InitialCreationScripts.sql
@@ -1,1 +1,24 @@
-﻿
+﻿CREATE SCHEMA app
+GO
+
+CREATE TABLE [app].[User] (
+    Id int NOT NULL PRIMARY KEY,	
+	FirstName varchar(50) NOT NULL,
+    LastName varchar(50) NOT NULL,
+	Email varchar(50),
+	PasswordHash varchar(250),
+	Phone varchar(50),
+	UserRoleId int ,
+	FailedTryCount smallint,
+	CreatedOn DateTime,
+	UpdatedOn DateTime
+);
+GO
+
+CREATE TABLE [app].[UserRole] (
+    Id int NOT NULL PRIMARY KEY,
+    RoleType smallint NOT NULL ,
+    UserId int NOT NULL,
+	CreatedOn DateTime,
+	UpdatedOn DateTime
+);
